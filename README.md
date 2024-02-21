51b1f836e52e43f3674576401d7ce7a4b8f55581
Alan Matoso <alan.matoso@acct.global>
2024-02-21 13:19:00 -0300

    refactor: change structure to clean code

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+# HarryVerse Landing Page
 
-## Getting Started
+Follow theses steps bellow to install and develop in HarryVerse repository. 
+This project has currently been published in https://alanmatoso-lp-harrypotter.vercel.app/
+
+## Configuration
+
+### Step 1 - Basic setup
 
 First, run the development server:
 
@@ -20,6 +25,37 @@ You can start editing the page by modifying `app/page.tsx`. The page auto-update
 
 This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
 
+### Step 2 - Cloning the Github IO repository
+
+[Clone](https://github.com/alanmatoso/alanmatoso.lp.harrypotter.git) this repository to your local files to be able to effectively start working on it.
+
+Then, access the repository's directory using your terminal.
+
+### Step 3 - Update your documents
+
+- Inside the project that was cloned on your machine, run the command below to update it properly and start working on it.
+- Command to update your project:
+  - command: `git pull origin develop`
+- If you point out any errors, it will be necessary to be performing the merge corrections.
+
+### Step 4 - Create branch
+
+- Create the branch for the task you are going to perform.
+- Branch name always in lowercase and with gitflow prefixes (feature, release, storyfix, etc.)
+- When creating a new branch always pull updates from develop before starting it.
+- Command for if the branch does not exist:
+  - command: `git checkout -b (prefixes/task-number)`
+  - ex: `git checkout -b feature/task-000`
+- Command for if the branch already exists:
+  - command: `git checkout (prefixes/task-number)`
+  - ex: `git checkout feature/task-000`
+
+## Deploy on Vercel
+
+The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
+
+Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+
 ## Learn More
 
 To learn more about Next.js, take a look at the following resources:
@@ -28,9 +64,3 @@ To learn more about Next.js, take a look at the following resources:
 - [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
 
 You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,8 @@
 "use client";
 
-import Image from "next/image";
 import React, { useState } from "react";
-
-interface Character {
-  id: string;
-  name: string;
-  alternate_names: string[];
-  species: string;
-  gender: string;
-  house: string;
-  dateOfBirth: string;
-  yearOfBirth: number;
-  wizard: boolean;
-  ancestry: string;
-  eyeColour: string;
-  hairColour: string;
-  wand: {
-    wood: string;
-    core: string;
-    length: number;
-  };
-  patronus: string;
-  hogwartsStudent: boolean;
-  hogwartsStaff: boolean;
-  actor: string;
-  alternate_actors: string[];
-  alive: boolean;
-  image: string;
-}
+import CharacterCard from "./components/CharacterCard";
+import { Character } from "./components/interfaces";
 
 export default function Home() {
   const [characters, setCharacters] = useState<Character[]>([]);
@@ -67,7 +41,7 @@ export default function Home() {
         <>
           <div className="page-tittle">HarryVerse</div>
           <div className="page-subtittle">
-            Dive Deep into Harry Potter's Characters
+            Dive Deep into Harry Potter Characters
           </div>
 
           <div className="cards-container">
@@ -76,161 +50,7 @@ export default function Home() {
               disabled={currentIndex === 0}
               className="nav-previus-button"
             />
-            <div className="character-card">
-              <Image
-                src={
-                  characters[currentIndex].image
-                    ? characters[currentIndex].image
-                    : "https://alanmatoso-lp-harrypotter.vercel.app/assets/profile-photo-error.png"
-                }
-                alt={characters[currentIndex].name}
-                className="character-image"
-                width={800}
-                height={600}
-                priority
-              />
-              <div className="character-details">
-                <h2 className="character-name">
-                  {characters[currentIndex].name}
-                </h2>
-                {characters[currentIndex].alternate_names.length > 0 && (
-                  <p className="character-attribute">
-                    Alternate Names:{" "}
-                    <span className="character-info">
-                      {characters[currentIndex].alternate_names.join(", ")}
-                    </span>
-                  </p>
-                )}
-                {characters[currentIndex].species && (
-                  <p className="character-attribute">
-                    Species:{" "}
-                    <span className="character-info">
-                      {characters[currentIndex].species}
-                    </span>
-                  </p>
-                )}
-                {characters[currentIndex].gender && (
-                  <p className="character-attribute">
-                    Gender:{" "}
-                    <span className="character-info">
-                      {characters[currentIndex].gender}
-                    </span>
-                  </p>
-                )}
-                {characters[currentIndex].house && (
-                  <p className="character-attribute">
-                    House:{" "}
-                    <span className="character-info">
-                      {characters[currentIndex].house}
-                    </span>
-                  </p>
-                )}
-                {characters[currentIndex].dateOfBirth && (
-                  <p className="character-attribute">
-                    Date of Birth:{" "}
-                    <span className="character-info">
-                      {characters[currentIndex].dateOfBirth}
-                    </span>
-                  </p>
-                )}
-                {characters[currentIndex].yearOfBirth && (
-                  <p className="character-attribute">
-                    Year of Birth:{" "}
-                    <span className="character-info">
-                      {characters[currentIndex].yearOfBirth}
-                    </span>
-                  </p>
-                )}
-                <p className="character-attribute">
-                  Wizard:{" "}
-                  <span className="character-info">
-                    {characters[currentIndex].wizard ? "Yes" : "No"}
-                  </span>
-                </p>
-                {characters[currentIndex].ancestry && (
-                  <p className="character-attribute">
-                    Ancestry:{" "}
-                    <span className="character-info">
-                      {characters[currentIndex].ancestry}
-                    </span>
-                  </p>
-                )}
-                {characters[currentIndex].eyeColour && (
-                  <p className="character-attribute">
-                    Eye Colour:{" "}
-                    <span className="character-info">
-                      {characters[currentIndex].eyeColour}
-                    </span>
-                  </p>
-                )}
-                {characters[currentIndex].hairColour && (
-                  <p className="character-attribute">
-                    Hair Colour:{" "}
-                    <span className="character-info">
-                      {characters[currentIndex].hairColour}
-                    </span>
-                  </p>
-                )}
-                {characters[currentIndex].wand && (
-                  <p className="character-attribute">
-                    Wand: Wood -{" "}
-                    <span className="character-info">
-                      {characters[currentIndex].wand.wood}
-                    </span>
-                    , Core -{" "}
-                    <span className="character-info">
-                      {characters[currentIndex].wand.core}
-                    </span>
-                    , Length -{" "}
-                    <span className="character-info">
-                      {characters[currentIndex].wand.length}
-                    </span>
-                  </p>
-                )}
-                {characters[currentIndex].patronus && (
-                  <p className="character-attribute">
-                    Patronus:{" "}
-                    <span className="character-info">
-                      {characters[currentIndex].patronus}
-                    </span>
-                  </p>
-                )}
-                <p className="character-attribute">
-                  Hogwarts Student:{" "}
-                  <span className="character-info">
-                    {characters[currentIndex].hogwartsStudent ? "Yes" : "No"}
-                  </span>
-                </p>
-                <p className="character-attribute">
-                  Hogwarts Staff:{" "}
-                  <span className="character-info">
-                    {characters[currentIndex].hogwartsStaff ? "Yes" : "No"}
-                  </span>
-                </p>
-                {characters[currentIndex].actor && (
-                  <p className="character-attribute">
-                    Actor:{" "}
-                    <span className="character-info">
-                      {characters[currentIndex].actor}
-                    </span>
-                  </p>
-                )}
-                {characters[currentIndex].alternate_actors.length > 0 && (
-                  <p className="character-attribute">
-                    Alternate Actors:{" "}
-                    <span className="character-info">
-                      {characters[currentIndex].alternate_actors.join(", ")}
-                    </span>
-                  </p>
-                )}
-                <p className="character-attribute">
-                  Alive:{" "}
-                  <span className="character-info">
-                    {characters[currentIndex].alive ? "Yes" : "No"}
-                  </span>
-                </p>
-              </div>
-            </div>
+            <CharacterCard character={characters[currentIndex]} />
             <button
               onClick={handleNext}
               disabled={currentIndex === characters.length - 1}
