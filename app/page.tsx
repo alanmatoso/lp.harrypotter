695e1a42a516ca42bbf4758a5794134288952ae4
Alan Matoso <alan.matoso@acct.global>
2024-02-20 23:33:17 -0300

    feat: created a first structure to get api data and show to endusers

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,153 @@
+"use client";
+
 import Image from "next/image";
+import React, { useState, useEffect } from "react";
+
+interface Character {
+  id: string;
+  name: string;
+  alternate_names: string[];
+  species: string;
+  gender: string;
+  house: string;
+  dateOfBirth: string;
+  yearOfBirth: number;
+  wizard: boolean;
+  ancestry: string;
+  eyeColour: string;
+  hairColour: string;
+  wand: {
+    wood: string;
+    core: string;
+    length: number;
+  };
+  patronus: string;
+  hogwartsStudent: boolean;
+  hogwartsStaff: boolean;
+  actor: string;
+  alternate_actors: string[];
+  alive: boolean;
+  image: string;
+}
 
 export default function Home() {
+  const [characters, setCharacters] = useState<Character[]>([]);
+  const [currentIndex, setCurrentIndex] = useState<number>(0);
+
+  const fetchCharacters = async () => {
+    try {
+      const response = await fetch(
+        "https://hp-api.onrender.com/api/characters"
+      );
+      if (response.ok) {
+        const data = await response.json();
+        setCharacters(data);
+      } else {
+        throw new Error("Failed to fetch characters");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  fetchCharacters();
+
+  const handleNext = () => {
+    setCurrentIndex((prevIndex: number) =>
+      Math.min(prevIndex + 1, characters.length - 1)
+    );
+  };
+
+  const handlePrev = () => {
+    setCurrentIndex((prevIndex: number) => Math.max(prevIndex - 1, 0));
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <div className="flex min-h-screen flex-col items-center justify-between p-24">
+      {characters.length > 0 && (
+        <>
+          <button onClick={handlePrev} disabled={currentIndex === 0}>
+            Previous
+          </button>
+          <div className="character-card">
+            <img
+              src={characters[currentIndex].image}
+              alt={characters[currentIndex].name}
+              className="character-image"
             />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+            <div className="character-details">
+              <h2 className="character-name">
+                {characters[currentIndex].name}
+              </h2>
+              <p className="character-info">
+                Alternate Names:{" "}
+                {characters[currentIndex].alternate_names.join(", ")}
+              </p>
+              <p className="character-info">
+                Species: {characters[currentIndex].species}
+              </p>
+              <p className="character-info">
+                Gender: {characters[currentIndex].gender}
+              </p>
+              <p className="character-info">
+                House: {characters[currentIndex].house}
+              </p>
+              <p className="character-info">
+                Date of Birth: {characters[currentIndex].dateOfBirth}
+              </p>
+              <p className="character-info">
+                Year of Birth: {characters[currentIndex].yearOfBirth}
+              </p>
+              <p className="character-info">
+                Wizard: {characters[currentIndex].wizard ? "Yes" : "No"}
+              </p>
+              <p className="character-info">
+                Ancestry: {characters[currentIndex].ancestry}
+              </p>
+              <p className="character-info">
+                Eye Colour: {characters[currentIndex].eyeColour}
+              </p>
+              <p className="character-info">
+                Hair Colour: {characters[currentIndex].hairColour}
+              </p>
+              <p className="character-info">
+                Wand: Wood - {characters[currentIndex].wand.wood}, Core -{" "}
+                {characters[currentIndex].wand.core}, Length -{" "}
+                {characters[currentIndex].wand.length}"
+              </p>
+              <p className="character-info">
+                Patronus: {characters[currentIndex].patronus}
+              </p>
+              <p className="character-info">
+                Hogwarts Student:{" "}
+                {characters[currentIndex].hogwartsStudent ? "Yes" : "No"}
+              </p>
+              <p className="character-info">
+                Hogwarts Staff:{" "}
+                {characters[currentIndex].hogwartsStaff ? "Yes" : "No"}
+              </p>
+              <p className="character-info">
+                Actor: {characters[currentIndex].actor}
+              </p>
+              {characters[currentIndex].alternate_actors.length > 0 && (
+                <p className="character-info">
+                  Alternate Actors:{" "}
+                  {characters[currentIndex].alternate_actors.join(", ")}
+                </p>
+              )}
+              <p className="character-info">
+                Alive: {characters[currentIndex].alive ? "Yes" : "No"}
+              </p>
+            </div>
+            <div className="navigation"></div>
+          </div>
+          <button
+            onClick={handleNext}
+            disabled={currentIndex === characters.length - 1}
+          >
+            Next
+          </button>
+        </>
+      )}
+    </div>
   );
 }
